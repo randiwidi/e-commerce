7bcfe075b26e23a2484e3c30d0fa4fd906510d86
randiwidi <randiwidi@gmail.com>
2019-05-15 14:10:12 +0700

    update baru

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['get','post'],'/admin','AdminController@login');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
